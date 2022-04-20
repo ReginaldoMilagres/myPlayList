6a783dadf8357bcee721810ec537c6675894e1d8
Reginaldo-Milagres <reginaldo.milagres@gmail.com>
2022-04-20 08:28:27 -0300

    getAll

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ app.use(express.urlencoded())
 
 connectToDb();
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const playlist = await Music.find()
+  /* console.log(playlist) */ // paramostra no console
+  res.render("index", {playlist});
 });
 
 app.get("/admin", (req, res) => {
